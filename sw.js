7395054da40d8c864788104d505ddc94fc20eac9
brentgotte <brent.gotte@gmail.com>
2022-12-06 10:12:23 +0100

    Base web-app

--- sw.js
+++ sw.js
@@ -1,26 +1,6 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'image',
+  workbox.strategies.cacheFirst()
+);
